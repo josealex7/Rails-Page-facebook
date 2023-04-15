d98448d985e80a4eff6a77efea93d90848cf7d9f
josealex7 <alevx197715@gmail.com>
2023-04-15 00:07:18 -0500

    add code javascript

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,18 @@ import "jquery_ujs"
 import "./jquery_ui"
 import "popper"
 import "bootstrap"
+
+$(document).on("ajax:success", "#edit_user_form", function(event) {
+    $("#editModal").modal("hide"); // cierra el modal
+    location.reload(); // recarga la página
+});
+
+$(document).on('turbo:load', function() {
+    $('.imagenPortada').click(function () {
+        let image_picture_id = $(this).attr('id');
+        console.log(image_picture_id);
+        $('input#portada_picture_id').val(image_picture_id);
+        $('li').removeClass('selected');
+        $(this).addClass('selected')
+    });
+});
