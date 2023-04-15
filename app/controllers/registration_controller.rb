2c5c0862550747ac4aca2bc26733c6b27292cae0
josealex7 <alevx197715@gmail.com>
2023-04-15 00:12:27 -0500

    change registration views

--- app/controllers/registration_controller.rb
+++
@@ -1,9 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-
-    private
-  
-    def sign_up_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :first_name, :last_name, :birthday, :sex)
-    end
-  
-  end
