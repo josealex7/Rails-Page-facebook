5376194bf8a880c0553d704b5454d042cefbeb4f
josealex7 <alevx197715@gmail.com>
2023-05-05 09:16:32 -0500

    add all methods in Publications controller

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,8 +1,68 @@
 class PublicationsController < ApplicationController
     def show
-        @user = User.find(params[:user_id])
-        @image = Image.find(params[:image_id])
-        @publication = Publication.where(user_id: @user.id, image_id: @image.id).first
-        render 'publications/show', layout: false
+        @publication = Publication.find(params[:id])
+        @image = Image.find(@publication.image_id)
+        @user = User.find(@publication.user_id)
+        render 'publications/show'
     end
+
+    def new
+        @publication = Publication.new
+    end
+    
+    def create
+        @publication = Publication.new
+        @publication.user_id = current_user.id
+
+        if params[:image].present?
+            image = Image.new(image_params)
+            image.name = params[:name]
+            image.user_id = current_user.id
+            image.url = upload_image_to_cloudinary
+
+            if image.save
+                @publication.image = image
+                @publication.description = image.name
+            else
+                flash[:error] = "Image could not be saved"
+                render :new and return
+            end
+
+        elsif params[:name].blank?
+
+            flash[:error] = "Description or Image is required"
+            render :new and return
+
+        else
+            @publication.description = params[:name]
+        end
+
+        if @publication.save && params[:image].present?
+            redirect_to publication_path(@publication.id), notice: 'Publication was successfully created.'
+        elsif !params[:name].blank?
+            redirect_to user_path(current_user)
+        else
+            render :new
+        end
+    end
+
+
+    def update_description
+        @publication = Publication.find(params[:publication_id])
+        @publication.update(description: params[:description])
+        head :no_content
+    end
+    
+    private
+
+    def image_params
+        params.require(:image).permit(:name, :url)
+    end
+
+    def upload_image_to_cloudinary
+        tempfile = params[:image][:url].tempfile
+        upload_result = Cloudinary::Uploader.upload(tempfile.path)
+        upload_result['secure_url']
+    end
+
 end
