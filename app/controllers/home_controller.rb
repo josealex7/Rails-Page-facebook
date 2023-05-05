0be5f1a8b0cfd202e5e982f311d939317a8cd860
josealex7 <alevx197715@gmail.com>
2023-05-05 09:13:58 -0500

    add method index and load_more_publication in controller Home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,19 @@
 class HomeController < ApplicationController
     def index
-
+        @friends = current_user.friends
+        @publications = Publication.where(user: [current_user] + @friends).where('created_at >= ?', 3.days.ago).order(created_at: :desc)
+        @histories = History.where(user: current_user)
     end
+
+    def load_more_publications
+        @publications = Publication.where(user: [current_user] + @friends).where('created_at >= ?', 3.days.ago).order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+        respond_to do |format|
+          format.js
+        end
+    end
+
+    def show_games
+        render 'home/videogames'
+    end
+
 end
