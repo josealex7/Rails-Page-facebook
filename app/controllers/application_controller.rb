870e9558292b208cbdf128125210d45428e82140
josealex7 <alevx197715@gmail.com>
2023-05-05 09:12:27 -0500

    add method set_lats_messages in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,21 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
     before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :set_last_messages
+
+    def set_last_messages
+        @last_messages = {}
+        if current_user
+          friends = current_user.friends
+          friends.each do |friend|
+            last_message = Message.where(sender: current_user, receiver: friend)
+                                   .or(Message.where(sender: friend, receiver: current_user))
+                                   .order(created_at: :desc)
+                                   .first
+            @last_messages[friend.id] = last_message
+          end
+        end
+    end
 
 
     protected
@@ -9,5 +24,7 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up, keys: [:sex, :birthday, :first_name, :last_name])
         devise_parameter_sanitizer.permit(:account_update, keys: [:sex, :birthday, :first_name, :last_name])
     end
+
+    
 
 end
