b8969506d36c73c0c3d26a10b5e08412c9406978
josealex7 <alevx197715@gmail.com>
2023-05-05 22:01:28 -0500

    add validate image_url in history

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,54 +1,5 @@
 class History < ApplicationRecord
     belongs_to :user
 
-
-
-    # MONTHS_ESP = {
-    #     "January" => "Enero",
-    #     "February" => "Febrero",
-    #     "March" => "Marzo",
-    #     "April" => "Abril",
-    #     "May" => "Mayo",
-    #     "June" => "Junio",
-    #     "July" => "Julio",
-    #     "August" => "Agosto",
-    #     "September" => "Septiembre",
-    #     "October" => "Octubre",
-    #     "November" => "Noviembre",
-    #     "December" => "Diciembre"
-    #   }
-
-    # def date_publication
-    #     month_name = MONTHS_ESP[self.created_at.strftime("%B")]
-    #     self.created_at.strftime("%d de #{month_name} del %Y")
-    # end
-
-    # def user_not_liked(current_user)
-    #     like = Like.find_by(user_id: current_user.id, publication_id: self.id)
-    #     !like.is_like
-    # end
-
-    # def user_liked(current_user)
-    #     return false if !exist_liked(current_user)
-    #     like = Like.find_by(user_id: current_user.id, publication_id: self.id)
-    #     return like.is_like
-    # end
-
-    # def exist_liked(current_user)
-    #     like = Like.find_by(user_id: current_user.id, publication_id: self.id)
-    #     if like.nil?
-    #         return false
-    #     else
-    #         return true
-    #     end
-    # end
-
-    # def like_id(current_user)
-    #     like = Like.find_by(user_id: current_user.id, publication_id: self.id)
-    #     return like.id
-    # end
-
-    # def original
-    #     !publication_shares.any?
-    # end
+    validates :image_url, presence: true
 end
