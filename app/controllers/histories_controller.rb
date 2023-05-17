44eaea7038e87c563cbdd7a17459cda068c4760d
josealex7 <alevx197715@gmail.com>
2023-05-17 09:32:09 -0500

    refactoring histories controller

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,26 +1,19 @@
 class HistoriesController < ApplicationController
-    def create
-        @history = History.new
-        @history.user = current_user
-        if params[:image].present?
-            @history.image_url = upload_image_to_cloudinary
-            if @history.save
-                respond_to do |format|
-                    format.js { render :create_success }
-                  end
-            else
-                flash[:error] = "Image could not be saved"
-                render :new and return
-            end
-        else
-            flash[:error] = "Description or Image is required"
-        end
+  def create
+    unless params[:image].presence
+      flash[:error] = "Image is required"
+      return
     end
 
-    def upload_image_to_cloudinary
-        tempfile = params[:image][:url].tempfile
-        upload_result = Cloudinary::Uploader.upload(tempfile.path)
-        upload_result['secure_url']
+    @history = History.new(user: current_user, image_url: upload_image_to_cloudinary)
+    if @history.save
+      respond_to do |format|
+        format.js { render :create_success }
+      end
+    else
+      flash[:error] = "Image could not be saved"
+      render :new
     end
+  end
 
 end
