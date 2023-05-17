f98136853d5683f6f05828e79bfd508a4d2a40c0
josealex7 <alevx197715@gmail.com>
2023-05-17 09:31:05 -0500

    edit index for show_conversation in chatfriend

--- app/controllers/chatfriend_controller.rb
+++ app/controllers/chatfriend_controller.rb
@@ -1,7 +1,6 @@
 class ChatfriendController < ApplicationController
-    def index
-       @message = Message.new
-       @user_friend = User.find(params[:id])
+    def show_conversation
+       @user_friend = User.find(params[:user_id])
        @messages = Message.where(sender: current_user, receiver: @user_friend)
                                    .or(Message.where(sender: @user_friend, receiver: current_user))
                                    .order(created_at: :asc)
