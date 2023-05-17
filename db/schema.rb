b86e4d4e1f53ffd1ba8715f21a62426f6b92d100
josealex7 <alevx197715@gmail.com>
2023-05-17 10:21:37 -0500

    add customer to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_022107) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_16_163005) do
+  create_table "customers", force: :cascade do |t|
+    t.string "stripe_customer_id"
+    t.string "name"
+    t.string "email"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_customers_on_user_id"
+  end
+
   create_table "friendships", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "friend_id"
@@ -93,7 +103,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_022107) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "page@gmail.com", null: false
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "nopassword"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -123,6 +133,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_022107) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "customers", "users"
   add_foreign_key "friendships", "users"
   add_foreign_key "friendships", "users", column: "friend_id"
   add_foreign_key "histories", "users"
