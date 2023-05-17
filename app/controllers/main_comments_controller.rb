c26c53b1ca71351084e16ba8cd3b35bfd3e800ca
josealex7 <alevx197715@gmail.com>
2023-05-17 09:39:37 -0500

    refactoring maincomments controller

--- app/controllers/main_comments_controller.rb
+++ app/controllers/main_comments_controller.rb
@@ -1,27 +1,8 @@
 class MainCommentsController < ApplicationController
-    def create
-        @comment = MainComment.new
-        if !params[:text_comment].blank?
-            @comment.text_comment = params[:text_comment]
-            @comment.user = current_user
-            @comment.publication = Publication.find(params[:id])
-            @comment.save
-            redirect_to show_publication_path(@comment.publication.user,  image_id: @comment.publication.image_id )
-        else
-            head :no_content
-        end
-    end
+  def create
+    return head :no_content if params[:text_comment].blank?
 
-    def update
-        @like = Like.find_by(user_id: current_user.id, publication_id: Publication.find(params[:id]))
-        @like.update(is_like: !@like.is_like)
-        publication = Publication.find(params[:id])
-
-        data = {
-            likesCount: publication.likes.where(is_like: true).count,
-            add: @like.is_like,
-        }
-
-        render json: data
-    end
+    @comment = MainComment.create(text_comment: params[:text_comment], user: current_user, publication_id: params[:id])
+    redirect_to publication_path(params[:id])
+  end
 end
