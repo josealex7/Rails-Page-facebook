971168b4891660044d1d4ec64962172371e016e7
josealex7 <alevx197715@gmail.com>
2023-05-17 09:39:24 -0500

    refactoring controller likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,23 +1,19 @@
 class LikesController < ApplicationController
-    def create
-        @like = Like.new
-        @like.is_like = true
-        @like.user = current_user
-        @like.publication = Publication.find(params[:id])
-        @like.save
-        head :no_content
-    end
+  def create
+    Like.create(is_like: true, user: current_user, publication: Publication.find(params[:id]))
+    head :no_content
+  end
 
-    def update
-        @like = Like.find_by(user_id: current_user.id, publication_id: Publication.find(params[:id]))
-        @like.update(is_like: !@like.is_like)
-        publication = Publication.find(params[:id])
+  def update
+    @like = Like.find_by(user_id: current_user.id, publication_id: params[:id])
+    @like.update(is_like: !@like.is_like)
 
-        data = {
-            likesCount: publication.likes.where(is_like: true).count,
-            add: @like.is_like,
-        }
+    likes_count = Publication.find(params[:id]).likes.where(is_like: true).count
+    data = {
+      likesCount: likes_count,
+      add: @like.is_like,
+    }
 
-        render json: data
-    end
+    render json: data
+  end
 end
