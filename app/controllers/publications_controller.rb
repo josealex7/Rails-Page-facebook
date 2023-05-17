9676675eeadc66987da0c6c5dddaaed09ed1cd52
josealex7 <alevx197715@gmail.com>
2023-05-17 09:40:55 -0500

    refactoring publications controller

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,68 +1,48 @@
 class PublicationsController < ApplicationController
-    def show
-        @publication = Publication.find(params[:id])
-        @image = Image.find(@publication.image_id)
-        @user = User.find(@publication.user_id)
-        render 'publications/show'
-    end
-
-    def new
-        @publication = Publication.new
-    end
+  def show
+    @publication = Publication.find(params[:id])
+    @image = Image.find(@publication.image_id)
+    @user = User.find(@publication.user_id)
+    render 'publications/show'
+  end
     
-    def create
-        @publication = Publication.new
-        @publication.user_id = current_user.id
-
-        if params[:image].present?
-            image = Image.new(image_params)
-            image.name = params[:name]
-            image.user_id = current_user.id
-            image.url = upload_image_to_cloudinary
-
-            if image.save
-                @publication.image = image
-                @publication.description = image.name
-            else
-                flash[:error] = "Image could not be saved"
-                render :new and return
-            end
-
-        elsif params[:name].blank?
-
-            flash[:error] = "Description or Image is required"
-            render :new and return
-
-        else
-            @publication.description = params[:name]
-        end
-
-        if @publication.save && params[:image].present?
-            redirect_to publication_path(@publication.id), notice: 'Publication was successfully created.'
-        elsif !params[:name].blank?
-            redirect_to user_path(current_user)
-        else
-            render :new
-        end
+  def create
+    unless params[:image].present? || params[:name].present?
+      flash[:error] = "Description or Image is required"
     end
 
-
-    def update_description
-        @publication = Publication.find(params[:publication_id])
-        @publication.update(description: params[:description])
-        head :no_content
+    @publication = Publication.new(user_id: current_user.id)
+    if params[:image].present?
+      image = Image.new(name: params[:name], user_id: current_user.id, url: upload_image_to_cloudinary)
+      if image.save
+        @publication.image = image
+        @publication.description = image.name
+      else
+        flash[:error] = "Image could not be saved"
+        render :new and return
+      end
+    else
+      @publication.description = params[:name]
     end
+
+    if @publication.save && params[:image].present?
+      redirect_to publication_path(@publication.id), notice: 'Publication was successfully created.'
+    elsif !params[:name].blank?
+      redirect_to user_path(current_user)
+    else
+      render :new
+    end
+  end
+
+
+  def update_description
+    Publication.find(params[:publication_id]).update(description: params[:description])
+    head :no_content
+  end
     
-    private
+  private
 
-    def image_params
-        params.require(:image).permit(:name, :url)
-    end
-
-    def upload_image_to_cloudinary
-        tempfile = params[:image][:url].tempfile
-        upload_result = Cloudinary::Uploader.upload(tempfile.path)
-        upload_result['secure_url']
-    end
-
+  def image_params
+    params.require(:image).permit(:name, :url)
+  end
 end
