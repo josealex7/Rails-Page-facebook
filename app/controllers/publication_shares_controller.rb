5d2d44ccfa0ea2276bc9844a0bb568525c2741f8
josealex7 <alevx197715@gmail.com>
2023-05-17 09:40:35 -0500

    refactoring publicationshares controller

--- app/controllers/publication_shares_controller.rb
+++ app/controllers/publication_shares_controller.rb
@@ -1,13 +1,8 @@
 class PublicationSharesController < ApplicationController
-    def create
-        @publication = Publication.new
-        @publication.user = current_user
-        @publication.description = params[:description]
-        if @publication.save
-            @publication_shares = PublicationShare.new
-            @publication_shares.publication = @publication
-            @publication_shares.publicationshare = Publication.find(params[:id])
-            @publication_shares.save
-        end
+  def create
+    @publication = Publication.new(user: current_user, description: params[:description])
+    if @publication.save
+      PublicationShare.create(publication: @publication, publicationshare: Publication.find(params[:id]))
     end
+  end  
 end
