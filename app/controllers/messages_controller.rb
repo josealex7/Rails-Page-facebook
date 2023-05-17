843c90ce574a444e47bb3ef3519b719f7840f7d9
josealex7 <alevx197715@gmail.com>
2023-05-17 09:39:50 -0500

    refactoring messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,35 +1,21 @@
 class MessagesController < ApplicationController
-    def create
-      return false if params[:message].blank?
-      @message = Message.new
-      @message.message = params[:message]
-      @message.sender = current_user
-      @message.receiver_id = params[:receiver_id]
-      if @message.save
-        @image_url = current_user.image_profile_id ? Image.find(current_user.image_profile_id).url : 'no-image' 
-        isSimilar = params[:receiver_id] == current_user.id ? true : false;
-        html = message_render(@message)
-        ActionCable.server.broadcast("chatfriend_channel", { 
-          body: @message.message,
-          sender: current_user,
-          date_created: @message.created_at.strftime("%b %d, %Y %H:%M:%S"),
-          image_url: @image_url,
-          html: html
-        })
-        respond_to do |format|
-          format.js
-        end
-      end
+  def create
+    return false if params[:message].blank?
+    @message = Message.new(message_params.merge(sender:current_user))
+    if @message.save
+      @image_url = current_user.image_profile_id ? Image.find(current_user.image_profile_id).url : 'no-image' 
+      isSimilar = params[:receiver_id] == current_user.id ? true : false;
+      ActionCable.server.broadcast("chatfriend_channel", { 
+        body: @message.message,
+        sender: current_user,
+        image_url: @image_url
+      })
     end
-  
-    private
-  
-    def message_params
-      params.require(:message).permit(:message)
-    end
-  
-    def message_render(message)
-      ApplicationController.render(partial: 'messages/new_message', locals: { message: message })
-    end
-  
+  end
+
+  private
+
+  def message_params
+    params.permit(:receiver_id, :message)
+  end
 end
