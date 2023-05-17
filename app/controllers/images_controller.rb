03163fcc37dcc3747d656fc6e41c51d035b7cc62
josealex7 <alevx197715@gmail.com>
2023-05-17 09:39:11 -0500

    refactoring controller images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,44 +1,27 @@
 class ImagesController < ApplicationController
-    def new
-      @image = Image.new
-    end
-  
-    def create
-        if image_params.nil?
-            flash[:error] = "Image cannot be blank"
-            render :new and return
-        end
-
-        @image = Image.new(image_params)
-        @image.name = params[:name]
-        @image.user_id = current_user.id
-        @image.url = upload_image_to_cloudinary
-        if @image.save
-            create_publication(@image)
-            redirect_to @image, notice: 'Image was successfully uploaded.'
-        else
-            render :new
-        end
-    end
-  
-    private
-  
-    def image_params
-        params.require(:image).permit(:name, :url)
+  def create
+    if image_params.nil?
+      flash[:error] = "Image cannot be blank"
+      render :new and return
     end
 
-    def upload_image_to_cloudinary
-        tempfile = params[:image][:url].tempfile
-        upload_result = Cloudinary::Uploader.upload(tempfile.path)
-        upload_result['secure_url']
+    @image = Image.new(name: params[:name], user_id: current_user.id, url: upload_image_to_cloudinary)
+    if @image.save
+      create_publication(@image)
+      redirect_to @image, notice: 'Image was successfully uploaded.'
+    else
+      render :new
     end
+  end
+  
+  private
 
-    def create_publication(image)
-        @publication = Publication.new
-        @publication.description=image.name
-        @publication.image=image
-        @publication.user=current_user
-        @publication.save
-    end
+  def image_params
+    params.require(:image).permit(:name, :url)
+  end
+
+  def create_publication(image)
+    Publication.create(description: image.name, image: image, user: current_user)
+  end
 
 end
