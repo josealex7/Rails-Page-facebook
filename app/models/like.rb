295cab2f1447e00ae7f464bcfc7efac50b467d2e
josealex7 <alevx197715@gmail.com>
2023-05-23 16:37:10 -0500

    add validates in model like

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,4 +2,10 @@ class Like < ApplicationRecord
     belongs_to :user
     belongs_to :publication
 
+    validates :is_like, inclusion: { in: [true, false] }
+    validates :user_id, presence: true
+    validates :publication_id, presence: true
+    validates :user_id, uniqueness: { scope: :publication_id }
+    
+
 end
