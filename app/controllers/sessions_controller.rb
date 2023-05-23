a34b50acc169fc0e162d62e7cab58437d7bce6ac
josealex7 <alevx197715@gmail.com>
2023-05-23 16:34:35 -0500

    edit controllador sessión

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,11 @@
 class SessionsController < Devise::SessionsController
-    def new
-      # Agrega tu propia lógica para la acción "new"
+  def create
+    user = User.find_by(email: params[:user][:email])
+    if user && user.user_type == "page"
+        flash.now[:alert] = "Iniciar sesión no permitido para este tipo de usuario."
+        redirect_to root_path
+    else
       super
-    end
-  
-    def create
-        user = User.find_by(email: params[:user][:email])
-        
-        if user && user.user_type == "page"
-            puts '----------------------------------------------------------------'
-            puts user
-            flash.now[:alert] = "Iniciar sesión no permitido para este tipo de usuario."
-            redirect_to root_path
-        else
-          super
-        end
-        
-    end
-  
-    def destroy
-      # Agrega tu propia lógica para la acción "destroy"
-      super
-    end
+    end 
+  end
 end
