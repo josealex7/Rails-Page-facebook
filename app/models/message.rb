d502c48361c19131f2fd97c5b6280a3dbec9c566
josealex7 <alevx197715@gmail.com>
2023-05-23 16:38:14 -0500

    edit model MainComment

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,16 @@
 class Message < ApplicationRecord
     belongs_to :sender, class_name: "User"
     belongs_to :receiver, class_name: "User"
-  
-    validates :message, presence: true
+    
+    validates :message, presence: true, length: { maximum: 1000 }
     validates :sender, presence: true
     validates :receiver, presence: true
+
+    validate :sender_and_receiver_are_different
+
+    def sender_and_receiver_are_different
+    errors.add(:base, "El emisor y el receptor no pueden ser el mismo usuario") if sender_id == receiver_id
+    end
 
 
     def last_message(current_user, user)
