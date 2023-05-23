9a34d7bc0b85c92386cf28e8f36f3391daa3cd6a
josealex7 <alevx197715@gmail.com>
2023-05-23 16:39:09 -0500

    add validates publicationshare model

--- app/models/publication_share.rb
+++ app/models/publication_share.rb
@@ -1,4 +1,8 @@
 class PublicationShare < ApplicationRecord
-    belongs_to :publication
-    belongs_to :publicationshare, class_name: "Publication"
-  end
+  belongs_to :publication
+  belongs_to :publicationshare, class_name: "Publication"
+
+  validates :publication, presence: true
+  validates :publicationshare, presence: true
+
+end
