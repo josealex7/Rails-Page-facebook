c9ef832e51fe1eae362d1aeba78179efb8d04ef1
josealex7 <alevx197715@gmail.com>
2023-05-23 16:37:02 -0500

    add validates in model image

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,17 @@
 class Image < ApplicationRecord
     belongs_to :user
     has_many :publications
+
+    validates :url, presence: true, format: { without: /\A\s*\z/, message: "can't be blank" }
+    validates :url, uniqueness: true
+    validates :user_id, presence: true
+    validates :url, length: { maximum: 255 }
+    validates :url, format: { with: /\.(png|jpe?g|gif)\z/i, message: "must be a URL for a PNG, JPG, or GIF image" }
+    validate :check_url_is_image
+
+    private
+
+    def check_url_is_image
+        errors.add(:url, "must be a URL for a valid image") unless url.match?(/\Ahttps?:\/\/\S+\.(png|jpe?g|gif)\z/i)
+    end
 end
