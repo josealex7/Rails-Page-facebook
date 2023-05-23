ed7dd83c0cb75960b3462fc16fca99d900c2affa
josealex7 <alevx197715@gmail.com>
2023-05-23 16:36:52 -0500

    add validates in model History

--- app/models/history.rb
+++ app/models/history.rb
@@ -1,5 +1,9 @@
 class History < ApplicationRecord
     belongs_to :user
 
+    validates :image_url, length: { maximum: 255 }
+    validates :image_url, format: { with: /\.(png|jpg|jpeg)\z/i, message: "must be a URL for PNG, JPG or JPEG image" }
     validates :image_url, presence: true
+    validates :user_id, presence: true
+    
 end
