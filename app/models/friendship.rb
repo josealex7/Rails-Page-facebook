979f82b8974b66b55e4bd72f3fb74bdf4769db45
josealex7 <alevx197715@gmail.com>
2023-05-23 16:36:40 -0500

    add validates in model Friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,9 @@
 class Friendship < ApplicationRecord
-    belongs_to :user
-    belongs_to :friend, class_name: "User"
-  end
+  belongs_to :user
+  belongs_to :friend, class_name: "User"
+
+  validates :user_id, uniqueness: { scope: :friend_id }
+  validates :user, presence: true
+  validates :friend, presence: true
+
+end
